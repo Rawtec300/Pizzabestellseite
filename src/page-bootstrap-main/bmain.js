e920897691c56883e2d9881761a0e5de3e6153c8
Rawtec300 <Rawtec300@users.noreply.github.com>
2019-12-12 13:46:15 +0100

    hover funktion eingebaut

--- src/page-bootstrap-main/bmain.js
+++ src/page-bootstrap-main/bmain.js
@@ -1,17 +1,32 @@
 const basket_counter = document.querySelector('.basket-counter');
-const basket_icon = document.querySelector('.shopping-basket');
+const nav_basket_icon = document.querySelector('.shopping-basket');
 const btn_bestellen = document.querySelector('.btn-bestellen');
+const nav_icon_bg_color = nav_basket_icon.getAttribute('backgroundColor');
+
+
 let count = 0;
 
-function addItem(){
+function addItem() {
     count++;
     basket_counter.textContent = count;
 }
 
-function emptyBasket(){
+function emptyBasket() {
     count = 0;
     basket_counter.textContent = count;
 }
 
+function basketHover() {
+    nav_basket_icon.style.backgroundColor = 'grey';
+
+}
+
+function basketHoverOut() {
+    nav_basket_icon.style.backgroundColor = nav_icon_bg_color;
+}
+
 btn_bestellen.addEventListener('click', addItem);
-basket_icon.addEventListener('click', emptyBasket);
+nav_basket_icon.addEventListener('click', emptyBasket);
+
+nav_basket_icon.onmouseover = basketHover;
+nav_basket_icon.onmouseout = basketHoverOut;
